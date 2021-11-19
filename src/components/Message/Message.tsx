de1391f0bf2d3842f9bd1d6a62108302e7eb68ff
Abdul-Gafur <abdul.taramov2001@gmail.com>
2021-11-19 20:27:06 +0300

    Update Message.tsx

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -6,19 +6,29 @@ interface MessageProps {
   isMe?: boolean;
 }
 
-const MessageRoot = styled.div`
+const MessageRoot = styled.div<Omit<MessageProps, 'children'>>`
   display: flex;
   padding: 0.5rem;
+  ${({ isMe }) =>
+    !isMe
+      ? css`
+          justify-content: flex-start;
+        `
+      : css`
+          justify-content: flex-end;
+        `}
 `;
 
 const MessageText = styled.span<Omit<MessageProps, 'children'>>`
   white-space: pre-wrap;
+  word-break: break-word;
   padding: 0.5rem;
   background: ${({ theme }) => theme.colors.primary.light};
   border: 1px solid ${({ theme }) => theme.colors.primary.main};
   color: #fff;
+  max-width: 80%;
   ${({ isMe }) =>
-    isMe
+    !isMe
       ? css`
           border-radius: 15px 10px 10px 0;
         `
@@ -29,7 +39,7 @@ const MessageText = styled.span<Omit<MessageProps, 'children'>>`
 
 const Message: FC<MessageProps> = ({ children, isMe }) => {
   return (
-    <MessageRoot>
+    <MessageRoot isMe={isMe}>
       <MessageText isMe={isMe}>{children}</MessageText>
     </MessageRoot>
   );
